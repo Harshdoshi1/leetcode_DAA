1ac08d0797d5e2247e983a5e54961539f2025228
Harsh Doshi <115213620+Harshdoshi1@users.noreply.github.com>
2024-12-04 19:42:33 +0530

    Time: 12 ms (75.00%), Space: 56.3 MB (18.18%) - LeetHub

---
+++ 2825-make-string-a-subsequence-using-cyclic-increments/2825-make-string-a-subsequence-using-cyclic-increments.js
@@ -0,0 +1,23 @@
+var canMakeSubsequence = function(source, target) {
+    const srcLen = source.length;
+    const tgtLen = target.length;
+    let targetChar = target[0];
+    
+    let srcIdx = 0, tgtIdx = 0;
+    while (srcIdx < srcLen && tgtIdx < tgtLen) {
+        const srcChar = source[srcIdx];
+        
+        if (srcChar === targetChar || 
+            String.fromCharCode(srcChar.charCodeAt(0) + 1) === targetChar ||
+            (srcChar === 'z' && targetChar === 'a')) {
+            tgtIdx++;
+            if (tgtIdx < tgtLen) {
+                targetChar = target[tgtIdx];
+            }
+        }
+        
+        srcIdx++;
+    }
+    
+    return tgtIdx === tgtLen;
+};
